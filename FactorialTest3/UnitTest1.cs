32412d0d43cb6c5d9cb681a2e89e69ec2280141d
OKleg <ol.krol@yandex.ru>
2022-12-19 13:56:51 +0300

    add 3 tests

---
+++ FactorialTest3/UnitTest1.cs
@@ -0,0 +1,47 @@
+using NUnit.Framework;
+using FactorialNS;
+namespace FactorialTest3
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void FactorialTest0()
+        {
+            Assert.AreEqual(MyFactorial.Factorial(0), 1); ;
+        }
+
+        public void FactorialTest1()
+        {
+            Assert.AreEqual(MyFactorial.Factorial(1), 1); ;
+        }
+
+        public void FactorialTest2()
+        {
+            Assert.AreEqual(MyFactorial.Factorial(2), 2); ;
+        }
+        public void FactorialTest3()
+        {
+            Assert.AreEqual(MyFactorial.Factorial(3), 6); ;
+        }
+
+        public void FactorialTest4()
+        {
+            Assert.AreEqual(MyFactorial.Factorial(4), 24); ;
+        }
+
+        public void FactorialTest5()
+        {
+            Assert.AreEqual(MyFactorial.Factorial(5), 120); ;
+        }
+
+        public void FactorialTest6()
+        {
+            Assert.AreEqual(MyFactorial.Factorial(6), 720);
+        }
+    }
+}
